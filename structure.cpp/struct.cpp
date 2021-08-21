1aef8c8ebf4219791a5689c8e90011e4d9a3e37e
Belayneh Getachew <betenatefera@gmail.com>
2021-08-21 17:25:39 +0300

    added

--- structure.cpp/struct.cpp
+++ structure.cpp/struct.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 using namespace std;
 struct stdt
-{
-int stud_id;
-int age;
+{ 
+char name[20];
+int stud_id,age;
 float mark[5];
-   
+struct date{
+int dd,mm,yy;
+}dat;
 };
 int main()
 {
-stdt 
-s1={0x122,22,{89,89,89,78,87}};
-cout<<s1.stud_id<<endl<<s1.age<<endl<<s1.mark[0];
+
+stdt s1[2]; 
+s1[0]={"belayneh",122,22,{89,89,89,78,87}};
+s1[1]={12,32,{1,1,1,1,1}};
+s1[0].dat={12,12,2021};
+cout<<s1[0].dat.dd<<endl<<s1[0].dat.mm<<endl<<s1[0].dat.yy<<endl;
+cout<<s1[1].stud_id<<endl<<s1[1].name<<endl<<s1[1].age<<endl;
+for(int i=0;i<5;i++)
+cout<<s1[1].mark[0]<<"\t";
 }
