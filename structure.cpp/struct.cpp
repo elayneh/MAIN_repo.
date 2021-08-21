ae829d10da8d6eadc2e90d371dff9fe774ce509e
Belayneh Getachew <betenatefera@gmail.com>
2021-08-21 17:33:53 +0300

    test it

--- structure.cpp/struct.cpp
+++ structure.cpp/struct.cpp
@@ -14,7 +14,7 @@ int main()
 
 stdt s1[2]; 
 s1[0]={"belayneh",122,22,{89,89,89,78,87}};
-s1[1]={12,32,{1,1,1,1,1}};
+s1[1]={"temesgen",12,25,{1,1,1,1,1}};
 s1[0].dat={12,12,2021};
 cout<<s1[0].dat.dd<<endl<<s1[0].dat.mm<<endl<<s1[0].dat.yy<<endl;
 cout<<s1[1].stud_id<<endl<<s1[1].name<<endl<<s1[1].age<<endl;
